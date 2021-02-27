1d2ca42ed932ccc89335df9c318e6884385c7bf7
Josh Kaplinsky <kaplinsky@wisc.edu>
2021-02-26 21:07:28 -0600

    env vars

--- env.go
+++ env.go
@@ -1,20 +1,52 @@
 package env_go
 
 import (
+	"errors"
 	"log"
 	"os"
+	"reflect"
 )
 
 // Key-Value mapping of os.Environ
-var EnvVars map[string]string
+type EnvVars map[string]string
 
 type tag struct {
-	Keys     []string
+	Key      string
 	Default  string
 	Required bool
 }
 
-func GetEnv(v interface{}) {
+func GetEnv(env interface{}) []string {
 	environ := os.Environ()
 	log.Print(environ)
+	return environ
+}
+
+// Get Environ
+
+// Convert to EnvVars (k/v map)
+
+// Unmarshal to env struct{}
+
+func unmarshal(envVars EnvVars, env interface{}) error {
+	v := reflect.ValueOf(env)
+	if v.Kind() != reflect.Ptr || v.IsNil() {
+		return errors.New("invalid value")
+	}
+
+	// take value of each field
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		// if struct loop through struct to allow nested structs
+		field := v.Field(i)
+		if field.Kind() == reflect.Struct {
+			// struct recurse
+		}
+		// else take value based on tag, marshal to passed env interface{}
+
+		tField := t.Field()
+		envTag := tField.Tag.Get("env")
+	}
+
+	return nil
 }
